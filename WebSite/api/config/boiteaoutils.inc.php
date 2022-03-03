73473ec00fa2a10e960f1da70710bb267b412a37
georgeSrndr <georges.srndr@eduge.ch>
2022-03-03 07:57:16 +0100

    push

---
+++ WebSite/api/config/boiteaoutils.inc.php
@@ -0,0 +1,137 @@
+<?php
+/*
+  Date       : Février 2022
+  Auteur     : Adrian Baumgartner
+  Sujet      : Librairie de fonctions php
+ */
+
+require "constantesDB.inc.php";
+
+/**
+ * Connecteur de la base de données du .
+ * Le script meurt (die) si la connexion n'est pas possible.
+ * @staticvar PDO $dbc
+ * @return \PDOO
+ */
+function dbGameAdvice()
+{
+    static $pokedexConnector = null;
+
+    if ($pokedexConnector == null) {
+        try {
+            $pokedexConnector = new PDO('mysql: ' . DBNAME . ';hostname= ' . HOSTNAME, DBUSER, PASSWORD, array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::ATTR_PERSISTENT => true
+            ));
+        } catch (PDOException $Exception) {
+            // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+            // String.
+            error_log($Exception->getMessage());
+            error_log($Exception->getCode());
+            die('Could not connect to MySQL');
+        }
+    }
+    return $pokedexConnector;
+}
+
+/*
+ * Retourne les données d'un pokémon en fonction de son idGame
+ * @param mixed $idGame
+ * @return false|array 
+ */
+function readGame($idGame)
+{
+    static $ps = null;
+    $sql = 'SELECT nom, description, dateDeSortie, prix, image';
+    $sql .= ' FROM GameAdvice.Games';
+    $sql .= ' WHERE idGame = :IDGAME';
+
+    if ($ps == null) {
+        $ps = dbGameAdvice()->prepare($sql);
+    }
+    $answer = false;
+    try {
+        $ps->bindParam(':IDGAME', $idGame, PDO::PARAM_INT);
+
+        if ($ps->execute())
+            $answer = $ps->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $answer;
+}
+
+function readGames($from = 0, $offset = 50)
+{
+    static $ps = null;
+    $sql = 'SELECT g.nom, g.description, g.dateDeSortie, g.prix, g.image';
+    $sql .= ' FROM GameAdvice.Games as g';
+    $sql .= ' ORDER BY nom ASC LIMIT :FROM,:OFFSET;';
+
+    if ($ps == null) {
+        $ps = dbGameAdvice()->prepare($sql);
+    }
+    $answer = false;
+    try {
+        $ps->bindParam(':FROM', $from, PDO::PARAM_INT);
+        $ps->bindParam(':OFFSET', $offset, PDO::PARAM_INT);
+        if ($ps->execute())
+            $answer = $ps->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $answer;
+}
+
+function readUsers($from = 0, $offset = 50)
+{
+    static $ps = null;
+    $sql = 'SELECT u.nom, u.prenom, u.email, u.mdp, u.photoProfil';
+    $sql .= ' FROM GameAdvice.Users as u';
+    $sql .= ' ORDER BY nom ASC LIMIT :FROM,:OFFSET;';
+
+    if ($ps == null) {
+        $ps = dbGameAdvice()->prepare($sql);
+    }
+    $answer = false;
+    try {
+        $ps->bindParam(':FROM', $from, PDO::PARAM_INT);
+        $ps->bindParam(':OFFSET', $offset, PDO::PARAM_INT);
+        if ($ps->execute())
+            $answer = $ps->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $answer;
+}
+
+/*
+ * Ajoute un nouveau utilisateur avec ses paramètres
+ * @param mixed $typeMedia Le type du média
+ * @param mixed $nomMedia Le nom du média
+ * @param mixed $creationDate  La date de création du média
+ * @return bool true si réussi
+ */
+function createUser($nom, $prenom, $email, $mdp, $photoProfil)
+{
+    static $ps = null;
+    $sql = "INSERT INTO `GameAdvice `.`Games` (`nom`, `prenom`, `email`, `mdp`, `photoProfil`) ";
+    $sql .= "VALUES (:NOM, :PRENOM, :EMAIL, :MDP, :PHOTOPROFIL)";
+    if ($ps == null) {
+        $ps = dbGameAdvice()->prepare($sql);
+    }
+    $answer = false;
+    try {
+        $ps->bindParam(':NOM', $nom, PDO::PARAM_STR);
+        $ps->bindParam(':PRENOM', $prenom, PDO::PARAM_STR);
+        $ps->bindParam(':EMAIL', $email, PDO::PARAM_STR);
+        $ps->bindParam(':MDP', $mdp, PDO::PARAM_STR);
+        $ps->bindParam(':PHOTOPROFIL', $photoProfil, PDO::PARAM_STR);
+        $answer = $ps->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    return $answer;
+}
+
+?>
